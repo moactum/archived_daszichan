fcbb6e145465192c4e07501d6d9ed25b2154f5f6
Xinchun Liu <lospringliu@gmail.com>
2018-06-09 16:08:39 -0400

    translation good

--- local_settings.py
+++ local_settings.py
@@ -127,5 +127,8 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 100000
 DATA_UPLOAD_MAX_MEMORY_SIZE = 26214400
 LOGIN_URL='/api/auth/login/'
 #DATABASE_ROUTERS = ['dbrouter.dbrouters.DbPostgresqlRouter',]
-DEBUG = False
+DEBUG = True
 API_CHAIN3 = 'http://localhost:3003/api'
+LOCALE_PATHS = [
+	os.path.join(BASE_DIR, 'locale')
+]
